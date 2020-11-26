cbe656949b17fbb5152b7147ddcb73fb116db9aa
Frank Riccobono <F.Riccobono@gmail.com>
2020-11-26 17:54:15 +0000

    refactoring (I think...these changes were a long time ago)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ encoding: utf-8
 from __future__ import division
 from goose import Goose
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk import pos_tag
 from nltk.corpus import stopwords
 import urllib2
 import math
@@ -11,18 +12,12 @@ import math
 
 class Summarizer(object):
     def sentence_intersection(self, sent1, sent2):
-        sent1_tokens = [ w for w in word_tokenize(sent1) if w not in
-                        stopwords.words('english')]
-        sent2_tokens = [ w for w in word_tokenize(sent2) if w not in
-                        stopwords.words('english')]
+        normal = math.log(len(sent1) + len(sent2))
 
-        normal = math.log(len(sent1_tokens) + len(sent2_tokens))
-
-        return len(set(sent1_tokens).intersection(sent2_tokens)) / normal
+        return len(set(sent1).intersection(sent2)) / normal
 
     def rank_sentences(self, sentences):
         n = len(sentences)
-        print sentences[0][0]
         score =  [0 for x in xrange(n)]
 
         for i in range(0, n):
@@ -32,23 +27,43 @@ class Summarizer(object):
                     sentences[j])
         return score
 
+    def __init__(self, text):
+        self.text = text
+        self.sentences = sent_tokenize(text)
+        self.tokenized_sentences = [
+            [w for w in word_tokenize(sent) if w not in
+             stopwords.words('english') ] 
+            for sent in self.sentences]
 
 
-
-    def summarize(self, text):
-        sentences = sent_tokenize(text)
-        scores = self.rank_sentences(sentences)
+    def summarize(self, factor=0.4):
+        scores = self.rank_sentences(self.tokenized_sentences)
         sorted_scores = sorted(enumerate(scores), reverse=True,
                                key=lambda x:x[1])
-        print "Original Length: ", len(sentences)
-        sum_length = int(len(sentences) * 0.25)
-        print "New Length: ", sum_length
-        ranked_sentences = sorted(sorted_scores[:sum_length],
-                                   key=lambda x:x[0])
-        return [sentences[index] for index, _ in
-                ranked_sentences]
-    
 
+        sum_length = int(len(self.sentences) * factor)
+        
+        return [self.sentences[index] for index, _ in
+                sorted(sorted_scores[:sum_length],
+                                   key=lambda x:x[0])]
+    
+    def extract_keywords(self):
+        pos_tagged_sents = [pos_tag(sent) 
+                            for sent in self.tokenized_sentences]
+
+        words = [ (w, pos) for sent in pos_tagged_sents for w, pos in sent]
+        
+        graph = {}
+
+        for index, (word, pos) in enumerate(words):
+            if pos in ['NN', 'NNS', 'NNPS', 'NNP']:
+                start = max(0, index-2)
+                end = min(index+2, len(words))
+                graph[word] = ":".join([w for w, pos in words[start:end]])
+
+        keyword_candidates = graph.items()
+        return keyword_candidates
+    
 def get_text(article_url):
     goose = Goose()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
@@ -58,10 +73,11 @@ def get_text(article_url):
     return article.cleaned_text
 
 def main():
-    summarizer = Summarizer()
     article_url = raw_input('Enter a url: ')
+    summarizer = Summarizer(get_text(article_url))
 
-    print summarizer.summarize(get_text(article_url))
+    print summarizer.summarize()
+    print summarizer.extract_keywords()
 
 if __name__ == "__main__":
     main()
--- summary.py
+++ summary.py
@@ -13,14 +13,23 @@ def index():
 def test():
     s = Summarizer()
     text = get_text('http://www.techtimes.com/articles/60808/20150616/xbox-one-backward-compatibility-heres-an-initial-list-of-xbox-360-games-you-can-play.htm')
-    return "\n".join(s.summarize(text))
+    return "\n".join(s.summarize())
 
 @app.route('/demo', methods=['POST'])
 def demo():
     text = request.form["text"]
-    s = Summarizer()
-    sents = s.summarize(text)
-    return render_template("demo.html", sents=sents)
+    summ_len = float(request.form["len"])
+
+    s = Summarizer(text)
+    sents = s.summarize(summ_len)
+    keywords = s.extract_keywords()
+    len1 = len(s.sentences)
+    len2 = len(sents)
+    return render_template("demo.html", 
+                           keywords=keywords,
+                           sents=sents, 
+                           orig_length=len1,
+                           sum_length=len2)
 
 
 if __name__ == '__main__':
